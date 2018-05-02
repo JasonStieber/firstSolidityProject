57afe883629072ca54c570d58a2ef8428bdcff95
theenderweggin <jrstieber@gmail.com>
2018-05-02 16:28:18 -0700

    created two tests in mocha

---
+++ test/inbox.test.js
@@ -0,0 +1,53 @@
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
+const provider = ganache.provider();
+const web3 = new Web3(provider);
+const { interface, bytecode } = require('../compile');
+
+/*
+class Car {
+  park() {
+    return 'stopped';
+  }
+
+  drive() {
+    return 'vroom';
+  }
+}
+let car;
+beforeEach( () => {
+  car = new Car();
+})
+
+describe('does the Car class work?', () => {
+  it('does the park function work?', () =>{
+    assert.equal(car.park(), 'stopped');
+  });
+  it('it can drive', () => {
+    assert.equal(car.drive(), 'vroom');
+  })
+});
+*/
+let accounts;
+let inbox;
+beforeEach(async () => {
+  // Get a list of all accounts
+  accounts = await web3.eth.getAccounts();
+  // use one of those accoutn to deploy the contract
+  // the contract
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: ['Hi there Batman']})
+    .send({ from: accounts[0], gas: '1000000' })
+});
+
+describe('Inbox tests', () => {
+  it('does our contract deploy?', () => {
+    assert.ok(inbox.options.address);
+  });
+
+  it('it has a default message', async () => {
+    const message = await inbox.methods.message().call();
+    assert.equal(message, 'Hi there Batman');
+  })
+})
